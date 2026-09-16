83530717fb66bf1e72f3321400b511d841ebf18a
eumesmooliveira <deoliveiratech@gmail.com>
2026-09-16 15:52:30 -0400

    feat: add Mochawesome reports to Cypress CI

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,21 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
+  reporter: "cypress-mochawesome-reporter",
+
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    charts: true,
+    reportPageTitle: "Full-Cycle QA Suite - Test Report",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
 
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
+      return config;
     },
   },
 
