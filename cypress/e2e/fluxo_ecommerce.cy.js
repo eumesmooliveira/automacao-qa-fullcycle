7edb12ebf2b61252cb6418e2893e4fa8b9888d38
eumesmooliveira <deoliveiratech@gmail.com>
2026-09-17 14:22:36 -0400

    refactor: use fixtures for E2E test data

--- cypress/e2e/fluxo_ecommerce.cy.js
+++ cypress/e2e/fluxo_ecommerce.cy.js
@@ -6,9 +6,25 @@ import CheckoutPage from '../pages/CheckoutPage';
 describe('Automação E2E - Full-Cycle QA Suite', () => {
   const baseUrl = 'https://www.saucedemo.com/';
 
+  let users;
+  let checkout;
+
+  before(() => {
+    cy.fixture('users').then((data) => {
+      users = data;
+    });
+
+    cy.fixture('checkout').then((data) => {
+      checkout = data;
+    });
+  });
+
   describe('Autenticação', () => {
     it('CT02 - Deve exibir mensagem de erro ao tentar logar com senha inválida', () => {
-      LoginPage.login('standard_user', 'senha_errada');
+      LoginPage.login(
+        users.invalid.username,
+        users.invalid.password
+      );
 
       LoginPage.validateErrorMessage(
         'Epic sadface: Username and password do not match any user in this service'
@@ -16,7 +32,10 @@ describe('Automação E2E - Full-Cycle QA Suite', () => {
     });
 
     it('CT10 - Deve impedir login de usuário bloqueado', () => {
-      LoginPage.login('locked_out_user', 'secret_sauce');
+      LoginPage.login(
+        users.locked.username,
+        users.locked.password
+      );
 
       LoginPage.validateErrorMessage(
         'Epic sadface: Sorry, this user has been locked out.'
@@ -28,9 +47,13 @@ describe('Automação E2E - Full-Cycle QA Suite', () => {
 
   describe('Fluxo de compra', () => {
     it('CT01 - Deve realizar login e concluir a compra com sucesso (Happy Path)', () => {
-      LoginPage.login();
+      LoginPage.login(
+        users.standard.username,
+        users.standard.password
+      );
 
       InventoryPage.validateInventoryPage();
+
       InventoryPage.addBackpackToCart();
       InventoryPage.validateCartBadge(1);
       InventoryPage.openCart();
@@ -39,46 +62,53 @@ describe('Automação E2E - Full-Cycle QA Suite', () => {
       CartPage.validateProduct('Sauce Labs Backpack');
       CartPage.proceedToCheckout();
 
-      CheckoutPage.validateCheckoutInformationPage();
-
       CheckoutPage.fillCheckoutInformation(
-        'Felipe',
-        'Oliveira',
-        '78550000'
+        checkout.validCustomer.firstName,
+        checkout.validCustomer.lastName,
+        checkout.validCustomer.postalCode
       );
 
       CheckoutPage.continueCheckout();
+
       CheckoutPage.validateCheckoutOverviewPage();
       CheckoutPage.validateProduct('Sauce Labs Backpack');
+
       CheckoutPage.finishCheckout();
       CheckoutPage.validateCheckoutCompletePage();
     });
 
     it('CT11 - Deve adicionar e remover um produto do carrinho', () => {
-      LoginPage.login();
+      LoginPage.login(
+        users.standard.username,
+        users.standard.password
+      );
 
       InventoryPage.validateInventoryPage();
+
       InventoryPage.addBackpackToCart();
       InventoryPage.validateCartBadge(1);
       InventoryPage.openCart();
 
       CartPage.validateCartPage();
       CartPage.validateProduct('Sauce Labs Backpack');
+
       CartPage.removeBackpack();
       CartPage.validateEmptyCart();
     });
 
     it('CT12 - Deve validar campos obrigatórios no checkout', () => {
-      LoginPage.login();
+      LoginPage.login(
+        users.standard.username,
+        users.standard.password
+      );
 
       InventoryPage.validateInventoryPage();
+
       InventoryPage.addBackpackToCart();
       InventoryPage.openCart();
 
       CartPage.validateCartPage();
       CartPage.proceedToCheckout();
-
-      CheckoutPage.validateCheckoutInformationPage();
 
       CheckoutPage.continueCheckout();
 
@@ -86,29 +116,40 @@ describe('Automação E2E - Full-Cycle QA Suite', () => {
         'Error: First Name is required'
       );
 
-      CheckoutPage.fillFirstName('Felipe');
+      CheckoutPage.fillFirstName(
+        checkout.validCustomer.firstName
+      );
+
       CheckoutPage.continueCheckout();
 
       CheckoutPage.validateErrorMessage(
         'Error: Last Name is required'
       );
 
-      CheckoutPage.fillLastName('Oliveira');
+      CheckoutPage.fillLastName(
+        checkout.validCustomer.lastName
+      );
+
       CheckoutPage.continueCheckout();
 
       CheckoutPage.validateErrorMessage(
         'Error: Postal Code is required'
       );
 
-      CheckoutPage.validateCheckoutInformationPage();
+      cy.url().should('include', '/checkout-step-one.html');
     });
 
     it('CT13 - Deve adicionar múltiplos produtos e validar o carrinho', () => {
-      LoginPage.login();
+      LoginPage.login(
+        users.standard.username,
+        users.standard.password
+      );
 
       InventoryPage.validateInventoryPage();
+
       InventoryPage.addBackpackToCart();
       InventoryPage.addBikeLightToCart();
+
       InventoryPage.validateCartBadge(2);
       InventoryPage.openCart();
 
